4ebce3a172fd79e807a7905356f7e4e91492b5f7
Wonseok <ls4050@naver.com>
2022-02-25 14:49:00 +0900

    ch08 #message Converter update

--- ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
@@ -2,9 +2,15 @@ package com.poscoict.ch08.controller.api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api")
 public class ApiController {
 	
+	@ResponseBody
+	@RequestMapping("/text")
+	public String text() {
+		return "Text data";
+	}
 }
