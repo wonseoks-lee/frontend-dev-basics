e4dc27d816dac1c30c43f216676d491d42056acc
Wonseok <ls4050@naver.com>
2022-02-25 14:30:14 +0900

    ch08 #build

---
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
@@ -0,0 +1,10 @@
+package com.poscoict.ch08.controller.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ApiController {
+	
+}
